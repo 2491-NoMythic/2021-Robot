64b137fa05f58a08c8856856e061ae3d2bbac60f
Schirbling <jacksonschirber@gmail.com>
2021-03-11 19:19:58 -0600

    Added a new command and subsystem for intake
    and cleaned up some other stuff

--- src/main/java/com/frc2491/clank/subsystems/Intake.java
+++ src/main/java/com/frc2491/clank/subsystems/Intake.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -7,34 +7,22 @@
 
 package com.frc2491.clank.subsystems;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.frc2491.clank.Settings.Constants;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.frc2491.clank.Settings.Constants;
-import com.frc2491.clank.commands.intake.AutoIntake;
-
 public class Intake extends SubsystemBase {
-	CANSparkMax coreIntakeMotor;
-	/**
-	 * Creates a new Intake.
-	 */
+	
+	CANSparkMax intakeMotor;
+	
 	public Intake() {
-		coreIntakeMotor = new CANSparkMax(Constants.Intake.intakeMotorPort, MotorType.kBrushless);
-		
+		intakeMotor = new CANSparkMax(Constants.Intake.intakeMotorPort, MotorType.kBrushless);
 	}
 
-	public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        setDefaultCommand(new AutoIntake(this));
-    }
-
-	public void startIntakeMotor(double motorPercent) {
-		coreIntakeMotor.set(motorPercent);
-	}
-
-	public void stopIntakeMotor() {
-		coreIntakeMotor.set(0);
+	public void runIntakeMotor(double motorPercent){
+		intakeMotor.set(motorPercent);
 	}
 
 	@Override
